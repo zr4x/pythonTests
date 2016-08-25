dd88ba5947a355ae8e1e09df8dd9a38e2cf19088
zr4x <skorpiod@gmail.com>
2016-08-25 23:23:11 +0500

    Add new test( test_new_user_create) and new class for user form fill

--- test_add_group.py
+++ test_add_group.py
@@ -2,6 +2,8 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from group import *
+from user_form import *
+
 
 def is_alert_present(wd):
     try:
@@ -53,12 +55,86 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def opent_home_page(self, wd):
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def fill_new_user_form(self, wd, UserForm):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(UserForm.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(UserForm.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(UserForm.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(UserForm.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(UserForm.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(UserForm.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(UserForm.adress)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(UserForm.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(UserForm.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(UserForm.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").send_keys(UserForm.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(UserForm.email)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(UserForm.homepage)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(UserForm.byear)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[3]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(UserForm.ayear)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(UserForm.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(UserForm.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(UserForm.notes)
+        self.confirm_new_user(wd)
+
+    def confirm_new_user(self, wd):
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def add_new_user(self, wd):
+        wd.find_element_by_link_text("add new").click()
 
     def test_add_named_group(self):
         wd = self.wd
-        self.opent_home_page(wd)
+        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group("NewGroup", 'NewHeader', 'NewFooter'))
@@ -67,12 +143,32 @@ class test_add_group(unittest.TestCase):
 
     def test_empty_group(self):
         wd = self.wd
-        self.opent_home_page(wd)
+        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group('', '', ''))
         self.return_to_group_page(wd)
         self.logout(wd)
+
+
+    def test_new_user_create(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.add_new_user(wd)
+        self.fill_new_user_form(wd,
+                                UserForm(
+                                firstname="Nikolo", middlename="Nikolo",
+                                lastname="Nikolo", nickname="Nikolo",
+                                title="Nikolo", company="Nikolo",
+                                adress="Nikolo", home="Nikolo",
+                                mobile="Nikolo", work="Nikolo",
+                                fax="Nikolo", notes="Nikolo",
+                                email="Nikolo", homepage="Nikolo",
+                                byear="Nikolo", ayear="Nikolo",
+                                address2="Nikolo", phone2="Nikolo"
+                                ))
+        self.return_to_home_page(wd)
 
     def tearDown(self):
         self.wd.quit()
---
+++ user_form.py
@@ -0,0 +1,30 @@
+
+
+class UserForm:
+
+    def __init__(self, firstname, middlename,
+                 lastname, nickname, title, company,
+                 adress, home, mobile,
+                 work, fax,
+                 email, homepage,
+                 byear, ayear,
+                 address2, phone2,
+                 notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.adress = adress
+        self.home = home
+        self.mobile = mobile
+        self.work  = work
+        self.fax  = fax
+        self.email = email
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
