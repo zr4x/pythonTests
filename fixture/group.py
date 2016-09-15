1be9c38be3c3ed8dc460f8e1f90a71be89dec257
Nik <nik@Mac-mini-Nik.local>
2016-09-15 18:01:53 +0500

    добавил проверку на группы

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 class GroupHelper:
     def __init__(self, app):
         self.app = app
@@ -36,7 +38,6 @@ class GroupHelper:
         self.open_groups_page()
         self.select_first_group()
         wd.find_element_by_name("delete").click()
-        self.return_to_group_page()
 
     def modification_first(self, group):
         wd = self.app.wd
@@ -55,6 +56,17 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name = text, id = id))
+        return groups
+
 
 
 
--- tests/group/test_add_group.py
+++ tests/group/test_add_group.py
@@ -2,7 +2,10 @@ from model.group import *
 
 
 def test_add_named_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("NewGroup", "NewHeader", "NewFooter"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
 
 
 def test_empty_group(app):
