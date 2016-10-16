6b4b391c8b8b54524398787904639e694a5f4640
Nik <skorpiod@gmail.com>
2016-10-17 02:13:10 +0500

    add db test's

--- conftest.py
+++ conftest.py
@@ -4,25 +4,41 @@ import os.path
 import importlib
 import jsonpickle
 from fixture.application import Application
+from fixture.db import DbFixture
 
 
 fixture = None
 target = None
 
 
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, url=target["url"])
-    fixture.session.ensure_login(username=target["login"], password=target["pass"])
+        fixture = Application(browser=browser, url=web_config["url"])
+    fixture.session.ensure_login(username=web_config["login"], password=web_config["pass"])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password = db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -37,6 +53,7 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
+
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
