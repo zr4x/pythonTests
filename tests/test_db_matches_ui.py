2376dbe9dffd7387746475b02dd865c7197f3e9f
Nik <nik@Mac-mini-Nik.local>
2016-10-24 18:04:37 +0500

    dbasd

--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -6,4 +6,4 @@ def test_group_list(app, db):
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)     s
