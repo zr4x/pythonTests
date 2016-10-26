f2836b8af9f6b46eafc1111e61ee6072ee8dba87
Nik <nik@Mac-mini-Nik.local>
2016-10-26 19:44:15 +0500

    optional_ui test

--- conftest.py
+++ conftest.py
@@ -41,6 +41,12 @@ def db(request):
     return dbfixture
 
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
+
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
@@ -53,6 +59,7 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 
 def pytest_generate_tests(metafunc):
