ec5d6913a418a0f168dff85b82c1b824356b6cfa
Nikolay Kozlov <nik@iMac-Nikolay.Dlink>
2016-10-27 02:07:47 +0500

    Home Work 21

--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.user_form import UserForm
 from timeit import timeit
 
 
@@ -10,4 +11,16 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
 
     print(timeit(lambda: map(clean, db.get_group_list()), number=1))
-    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    db_list = map(clean, db.get_group_list())
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_user_list(app, db):
+    print(timeit(lambda: app.user.get_users_list(), number=1))
+    ui_list = app.user.get_users_list()
+
+    def clean(user):
+        return UserForm(id=user.id, firstname=user.firstname.strip(), lastname=user.lastname.strip())
+
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=UserForm.id_or_max) == sorted(db_list, key=UserForm.id_or_max)
